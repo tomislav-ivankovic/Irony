81db3b47da5cc585c41704922b41ff7c3f107fd2
Tomislav Ivanković <git@tomislav-ivankovic.from.hr>
2025-02-02 21:26:36 +0100

    Remote slice, small refactor.

--- src/os/remote_slice.zig
+++ src/os/remote_slice.zig
@@ -5,7 +5,7 @@ const Process = @import("process.zig").Process;
 
 pub fn RemoteSlice(comptime Element: type, comptime sentinel: ?Element) type {
     return struct {
-        process_handle: w32.HANDLE,
+        process: Process,
         address: usize,
         len: usize,
         test_allocation: if (builtin.is_test) *u8 else void,
@@ -13,23 +13,22 @@ pub fn RemoteSlice(comptime Element: type, comptime sentinel: ?Element) type {
         const Self = @This();
 
         pub fn create(
-            process: *const Process,
+            process: Process,
             data: if (sentinel) |s| [:s]const Element else []const Element,
         ) !Self {
-            const process_handle = process.handle;
             const full_length = if (sentinel != null) data.len + 1 else data.len;
             const size_in_bytes = full_length * @sizeOf(Element);
-            const address = w32.VirtualAllocEx(process_handle, null, size_in_bytes, .{
+            const address = w32.VirtualAllocEx(process.handle, null, size_in_bytes, .{
                 .COMMIT = 1,
                 .RESERVE = 1,
             }, .{ .PAGE_READWRITE = 1 }) orelse return error.OsError;
-            errdefer _ = w32.VirtualFreeEx(process_handle, address, 0, .RELEASE);
-            const success = w32.WriteProcessMemory(process_handle, address, @ptrCast(data), size_in_bytes, null);
+            errdefer _ = w32.VirtualFreeEx(process.handle, address, 0, .RELEASE);
+            const success = w32.WriteProcessMemory(process.handle, address, @ptrCast(data), size_in_bytes, null);
             if (success == 0) {
                 return error.OsError;
             }
             return Self{
-                .process_handle = process_handle,
+                .process = process,
                 .address = @intFromPtr(address),
                 .len = data.len,
                 .test_allocation = if (builtin.is_test) try std.testing.allocator.create(u8) else .{},
@@ -37,7 +36,7 @@ pub fn RemoteSlice(comptime Element: type, comptime sentinel: ?Element) type {
         }
 
         pub fn destroy(self: *const Self) !void {
-            const success = w32.VirtualFreeEx(self.process_handle, @ptrFromInt(self.address), 0, .RELEASE);
+            const success = w32.VirtualFreeEx(self.process.handle, @ptrFromInt(self.address), 0, .RELEASE);
             if (success == 0) {
                 return error.OsError;
             }
@@ -58,7 +57,7 @@ const isMemoryWriteable = @import("memory.zig").isMemoryWriteable;
 
 test "create should allocate memory and copy the data to it" {
     const data = [_]u32{ 1, 2, 3, 4, 5 };
-    const remote_slice = try RemoteSlice(u32, null).create(&Process.getCurrent(), &data);
+    const remote_slice = try RemoteSlice(u32, null).create(Process.getCurrent(), &data);
     defer remote_slice.destroy() catch unreachable;
     try testing.expectEqual(true, isMemoryWriteable(remote_slice.address, remote_slice.getSizeInBytes()));
     const pointer: *[data.len]u32 = @ptrFromInt(remote_slice.address);
@@ -67,7 +66,7 @@ test "create should allocate memory and copy the data to it" {
 
 test "create should copy sentinel value when sentinel value is provided" {
     const data = [_:6]u32{ 1, 2, 3, 4, 5 };
-    const remote_slice = try RemoteSlice(u32, 6).create(&Process.getCurrent(), &data);
+    const remote_slice = try RemoteSlice(u32, 6).create(Process.getCurrent(), &data);
     defer remote_slice.destroy() catch unreachable;
     try testing.expectEqual(true, isMemoryWriteable(remote_slice.address, remote_slice.getSizeInBytes()));
     const pointer: *[data.len + 1]u32 = @ptrFromInt(remote_slice.address);
@@ -76,7 +75,7 @@ test "create should copy sentinel value when sentinel value is provided" {
 
 test "destroy should free the allocated memory" {
     const data = [_]u32{ 1, 2, 3, 4, 5 };
-    const remote_slice = try RemoteSlice(u32, null).create(&Process.getCurrent(), &data);
+    const remote_slice = try RemoteSlice(u32, null).create(Process.getCurrent(), &data);
     try testing.expectEqual(true, isMemoryWriteable(remote_slice.address, remote_slice.getSizeInBytes()));
     try remote_slice.destroy();
     try testing.expectEqual(false, isMemoryWriteable(remote_slice.address, remote_slice.getSizeInBytes()));
@@ -84,14 +83,14 @@ test "destroy should free the allocated memory" {
 
 test "getSizeInBytes should should return the correct value when no sentinel value is provided" {
     const data = [_]u32{ 1, 2, 3, 4, 5 };
-    const remote_slice = try RemoteSlice(u32, null).create(&Process.getCurrent(), &data);
+    const remote_slice = try RemoteSlice(u32, null).create(Process.getCurrent(), &data);
     defer remote_slice.destroy() catch unreachable;
     try testing.expectEqual(@sizeOf(@TypeOf(data)), remote_slice.getSizeInBytes());
 }
 
 test "getSizeInBytes should should return the correct value when sentinel value is provided" {
     const data = [_:6]u32{ 1, 2, 3, 4, 5 };
-    const remote_slice = try RemoteSlice(u32, 6).create(&Process.getCurrent(), &data);
+    const remote_slice = try RemoteSlice(u32, 6).create(Process.getCurrent(), &data);
     defer remote_slice.destroy() catch unreachable;
     try testing.expectEqual(@sizeOf(@TypeOf(data)), remote_slice.getSizeInBytes());
 }
