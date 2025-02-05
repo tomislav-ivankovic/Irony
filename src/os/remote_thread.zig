6ce757f664e3e2e298beb32825939df4de5c8cbe
Tomislav Ivanković <git@tomislav-ivankovic.from.hr>
2025-02-05 23:21:36 +0100

    Remote thread: New error handling.

--- src/os/remote_thread.zig
+++ src/os/remote_thread.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const w32 = @import("win32").everything;
+const errorContext = @import("../error_context.zig").errorContext;
 const os = @import("root.zig");
 
 pub const RemoteThread = struct {
@@ -14,7 +15,11 @@ pub const RemoteThread = struct {
         start_routine: *const fn (parameter: usize) u32,
         parameter: usize,
     ) !Self {
-        const handle = w32.CreateRemoteThread(process.handle, null, 0, @ptrCast(start_routine), @ptrFromInt(parameter), 0, null) orelse return error.OsError;
+        const handle = w32.CreateRemoteThread(process.handle, null, 0, @ptrCast(start_routine), @ptrFromInt(parameter), 0, null) orelse {
+            errorContext().newFmt(null, "{}", os.OsError.getLast());
+            errorContext().append(error.OsError, "CreateRemoteThread returned null.");
+            return error.OsError;
+        };
         const test_allocation = if (builtin.is_test) try std.testing.allocator.create(u8) else .{};
         return Self{ .handle = handle, .test_allocation = test_allocation };
     }
@@ -22,6 +27,8 @@ pub const RemoteThread = struct {
     pub fn clean(self: *const Self) !void {
         const success = w32.CloseHandle(self.handle);
         if (success == 0) {
+            errorContext().newFmt(null, "{}", os.OsError.getLast());
+            errorContext().append(error.OsError, "CloseHandle returned 0.");
             return error.OsError;
         }
         if (builtin.is_test) {
@@ -32,11 +39,15 @@ pub const RemoteThread = struct {
     pub fn join(self: *const Self) !u32 {
         const return_code = w32.WaitForSingleObject(self.handle, w32.INFINITE);
         if (return_code == @intFromEnum(w32.WAIT_FAILED)) {
+            errorContext().newFmt(null, "{}", os.OsError.getLast());
+            errorContext().appendFmt(error.OsError, "WaitForSingleObject returned: {}", .{return_code});
             return error.OsError;
         }
         var exit_code: u32 = undefined;
         const success = w32.GetExitCodeThread(self.handle, &exit_code);
         if (success == 0) {
+            errorContext().newFmt(null, "{}", os.OsError.getLast());
+            errorContext().append(error.OsError, "GetExitCodeThread returned 0.");
             return error.OsError;
         }
         return exit_code;
