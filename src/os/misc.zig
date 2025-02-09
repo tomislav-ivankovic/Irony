8b3f65d003e90382f3c0e4ecac43d4d493aac2c6
Tomislav Ivanković <git@tomislav-ivankovic.from.hr>
2025-02-09 01:29:19 +0100

    Get full path.

--- src/os/misc.zig
+++ src/os/misc.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const w32 = @import("win32").everything;
-const errorContext = @import("../misc/root.zig").errorContext;
+const mics = @import("../misc/root.zig");
 const os = @import("root.zig");
 
 pub fn pathToFileName(path: []const u8) []const u8 {
@@ -21,6 +21,27 @@ pub fn pathToFileName(path: []const u8) []const u8 {
     }
 }
 
+pub fn getFullPath(full_path_buffer: *[os.max_file_path_length]u8, short_path: []const u8) !usize {
+    var short_path_buffer_utf16 = [_:0]u16{0} ** os.max_file_path_length;
+    const short_path_size = std.unicode.utf8ToUtf16Le(&short_path_buffer_utf16, short_path) catch |err| {
+        mics.errorContext().newFmt(err, "Failed to convert UTF8 string \"{s}\" to UTF16-LE.", .{short_path});
+        return err;
+    };
+    const short_path_utf16 = short_path_buffer_utf16[0..short_path_size :0];
+    var full_path_buffer_utf16: [os.max_file_path_length:0]u16 = undefined;
+    const full_path_size = w32.GetFullPathNameW(short_path_utf16, full_path_buffer_utf16.len, &full_path_buffer_utf16, null);
+    if (full_path_size == 0) {
+        mics.errorContext().newFmt(null, "{}", os.OsError.getLast());
+        mics.errorContext().append(error.OsError, "GetFullPathNameW returned 0.");
+        return error.OsError;
+    }
+    const full_path_utf16 = full_path_buffer_utf16[0..full_path_size];
+    return std.unicode.utf16LeToUtf8(full_path_buffer, full_path_utf16) catch |err| {
+        mics.errorContext().new(err, "Failed to convert UTF16-LE string to UTF8.");
+        return err;
+    };
+}
+
 pub fn setConsoleCloseHandler(onConsoleClose: *const fn () void) !void {
     const Handler = struct {
         var function: ?*const fn () void = null;
@@ -35,8 +56,8 @@ pub fn setConsoleCloseHandler(onConsoleClose: *const fn () void) !void {
     Handler.function = onConsoleClose;
     const success = w32.SetConsoleCtrlHandler(Handler.call, 1);
     if (success == 0) {
-        errorContext().newFmt(null, "{}", os.OsError.getLast());
-        errorContext().append(error.OsError, "SetConsoleCtrlHandler returned 0.");
+        mics.errorContext().newFmt(null, "{}", os.OsError.getLast());
+        mics.errorContext().append(error.OsError, "SetConsoleCtrlHandler returned 0.");
         return error.OsError;
     }
 }
@@ -52,4 +73,11 @@ test "pathToFileName should return correct value" {
     try testing.expectEqualStrings("test", pathToFileName("\\test"));
     try testing.expectEqualStrings("", pathToFileName(""));
     try testing.expectEqualStrings("", pathToFileName("\\"));
+}
+
+test "getFullPath should produce correct full" {
+    var buffer: [os.max_file_path_length]u8 = undefined;
+    const size = try getFullPath(&buffer, "./test_1/test_2/test_3.txt");
+    const full_path = buffer[0..size];
+    try testing.expectStringEndsWith(full_path, "\\test_1\\test_2\\test_3.txt");
 }
