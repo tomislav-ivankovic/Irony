85f399281508b2d1d13a482c6f163f3f8337357d
Tomislav Ivanković <git@tomislav-ivankovic.from.hr>
2025-02-27 17:14:46 +0100

    Present function hooking.

--- src/dll.zig
+++ src/dll.zig
@@ -4,6 +4,7 @@ const w32 = @import("win32").everything;
 const misc = @import("misc/root.zig");
 const log = @import("log/root.zig");
 const os = @import("os/root.zig");
+const memory = @import("memory/hooking.zig");
 
 pub const module_name = "irony.dll";
 
@@ -57,6 +58,14 @@ fn init() void {
         misc.errorContext().logError();
     }
 
+    std.log.debug("Initializing hooking...", .{});
+    if (memory.Hooking.init()) {
+        std.log.debug("Hooking initialized.", .{});
+    } else |err| {
+        misc.errorContext().new(err, "Failed to initialize hooking.");
+        misc.errorContext().logError();
+    }
+
     std.log.debug("Finding present function...", .{});
     const present = os.findPresentFunction() catch |err| {
         misc.errorContext().append(err, "Failed to find present function.");
@@ -65,11 +74,50 @@ fn init() void {
     };
     std.log.debug("Present function found: 0x{X}", .{@intFromPtr(present)});
 
+    std.log.debug("Creating the present hook...", .{});
+    present_hook = memory.Hook(
+        fn (*const w32.IDXGISwapChain, u32, u32) callconv(std.os.windows.WINAPI) w32.HRESULT,
+    ).create(present, onPresent) catch |err| {
+        misc.errorContext().new(err, "Failed to create present hook.");
+        misc.errorContext().logError();
+        return;
+    };
+    std.log.debug("Present hook created.", .{});
+
+    std.log.debug("Enabling present hook...", .{});
+    present_hook.?.enable() catch |err| {
+        misc.errorContext().new(err, "Failed to enable present hook.");
+        misc.errorContext().logError();
+        return;
+    };
+    std.log.debug("Present hook enabled.", .{});
+
     std.log.info("Initialization completed.", .{});
 }
 
 fn deinit() void {
     std.log.info("Running de-initialization...", .{});
+
+    std.log.debug("Destroying the present hook...", .{});
+    if (present_hook) |hook| {
+        if (hook.destroy()) {
+            present_hook = null;
+            std.log.debug("Present hook destroyed.", .{});
+        } else |err| {
+            misc.errorContext().new(err, "Failed destroy present hook.");
+            misc.errorContext().logError();
+        }
+    } else {
+        std.log.debug("Nothing to destroy.", .{});
+    }
+
+    std.log.debug("De-initializing hooking...", .{});
+    if (memory.Hooking.deinit()) {
+        std.log.debug("Hooking de-initialized.", .{});
+    } else |err| {
+        misc.errorContext().new(err, "Failed to de-initialize hooking.");
+        misc.errorContext().logError();
+    }
 
     std.log.info("Stopping file logging...", .{});
     file_logger.stop();
@@ -93,4 +141,15 @@ fn startFileLogging() !void {
         misc.errorContext().appendFmt(err, "Failed to start file logging with file path: {s}", .{file_path});
         return err;
     };
+}
+
+var present_hook: ?memory.Hook(fn (*const w32.IDXGISwapChain, u32, u32) callconv(std.os.windows.WINAPI) w32.HRESULT) = null;
+
+fn onPresent(
+    swap_chain: *const w32.IDXGISwapChain,
+    sync_interval: u32,
+    flags: u32,
+) callconv(@import("std").os.windows.WINAPI) w32.HRESULT {
+    std.log.debug("Present function called.", .{});
+    return present_hook.?.original(swap_chain, sync_interval, flags);
 }
