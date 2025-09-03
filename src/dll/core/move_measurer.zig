dbb1c6f4b2f6b9b7363c491044cc97055854065d
Tomislav Ivanković <git@tomislav-ivankovic.from.hr>
2025-09-04 01:49:40 +0200

    Move measurer tests.

--- src/dll/core/move_measurer.zig
+++ src/dll/core/move_measurer.zig
@@ -121,3 +121,160 @@ pub const MoveMeasurer = struct {
         return attack_range - max_projection;
     }
 };
+
+const testing = std.testing;
+
+test "should set min_attack_z, max_attack_z, attack_range, recovery_range to correct value at correct time" {
+    const hurtCylinders = struct {
+        fn call(y: f32) model.HurtCylinders {
+            return model.HurtCylinders.init(.{
+                .left_ankle = .{ .cylinder = .{ .center = .fromArray(.{ -1, y, 0 }), .radius = 1, .half_height = 1 } },
+                .right_ankle = .{ .cylinder = .{ .center = .fromArray(.{ 1, y, 0 }), .radius = 1, .half_height = 3 } },
+                .left_hand = .{ .cylinder = .{ .center = .fromArray(.{ -1, y, 2 }), .radius = 1, .half_height = 1 } },
+                .right_hand = .{ .cylinder = .{ .center = .fromArray(.{ 1, y, 2 }), .radius = 1, .half_height = 1 } },
+                .left_knee = .{ .cylinder = .{ .center = .fromArray(.{ -1, y, 1 }), .radius = 1, .half_height = 1 } },
+                .right_knee = .{ .cylinder = .{ .center = .fromArray(.{ 1, y, 1 }), .radius = 1, .half_height = 1 } },
+                .left_elbow = .{ .cylinder = .{ .center = .fromArray(.{ -1, y, 4 }), .radius = 1, .half_height = 1 } },
+                .right_elbow = .{ .cylinder = .{ .center = .fromArray(.{ 1, y, 4 }), .radius = 1, .half_height = 1 } },
+                .head = .{ .cylinder = .{ .center = .fromArray(.{ 0, y, 7 }), .radius = 1, .half_height = 1 } },
+                .left_shoulder = .{ .cylinder = .{ .center = .fromArray(.{ -1, y, 6 }), .radius = 1, .half_height = 1 } },
+                .right_shoulder = .{ .cylinder = .{ .center = .fromArray(.{ 1, y, 6 }), .radius = 1, .half_height = 1 } },
+                .upper_torso = .{ .cylinder = .{ .center = .fromArray(.{ 0, y - 1, 5 }), .radius = 3, .half_height = 1 } },
+                .left_pelvis = .{ .cylinder = .{ .center = .fromArray(.{ -1, y, 3 }), .radius = 1, .half_height = 3 } },
+                .right_pelvis = .{ .cylinder = .{ .center = .fromArray(.{ 1, y, 3 }), .radius = 1, .half_height = 1 } },
+            });
+        }
+    }.call;
+    const hitLines = struct {
+        fn call(array: anytype) model.HitLines {
+            if (@typeInfo(@TypeOf(array)) != .array) {
+                const coerced: [array.len]sdk.math.LineSegment3 = array;
+                return call(coerced);
+            }
+            if (array.len > model.HitLines.max_len) {
+                @compileError("Array length exceeds maximum allowed number of lines.");
+            }
+            var buffer: [model.HitLines.max_len]model.HitLine = undefined;
+            for (array, 0..) |line, index| {
+                buffer[index] = .{ .line = line, .flags = .{} };
+            }
+            return .{ .buffer = buffer, .len = array.len };
+        }
+    }.call;
+    var frames = [_]model.Frame{
+        .{ .players = .{ .{
+            .move_frame = 99,
+            .move_total_frames = 99,
+            .position = sdk.math.Vec3.fromArray(.{ 0, 0, 0 }),
+            .rotation = std.math.pi,
+            .hurt_cylinders = hurtCylinders(0),
+            .hit_lines = hitLines(.{}),
+        }, .{} } },
+        .{ .players = .{ .{
+            .move_frame = 1,
+            .move_total_frames = 5,
+            .position = sdk.math.Vec3.fromArray(.{ 0, 1, 0 }),
+            .rotation = 0.5 * std.math.pi,
+            .hurt_cylinders = hurtCylinders(1),
+            .hit_lines = hitLines(.{}),
+        }, .{} } },
+        .{ .players = .{ .{
+            .move_frame = 2,
+            .move_total_frames = 5,
+            .position = sdk.math.Vec3.fromArray(.{ 0, 2, 0 }),
+            .rotation = 0.5 * std.math.pi,
+            .hurt_cylinders = hurtCylinders(2),
+            .hit_lines = hitLines(.{
+                sdk.math.LineSegment3{ .point_1 = .fromArray(.{ 0, 4, 1 }), .point_2 = .fromArray(.{ 1, 5, 2 }) },
+                sdk.math.LineSegment3{ .point_1 = .fromArray(.{ 0, 0, 2 }), .point_2 = .fromArray(.{ -1, 0, 1 }) },
+            }),
+        }, .{} } },
+        .{ .players = .{ .{
+            .move_frame = 3,
+            .move_total_frames = 5,
+            .position = sdk.math.Vec3.fromArray(.{ 0, 3, 0 }),
+            .rotation = 0.5 * std.math.pi,
+            .hurt_cylinders = hurtCylinders(3),
+            .hit_lines = hitLines(.{
+                sdk.math.LineSegment3{ .point_1 = .fromArray(.{ 0, 5, 2 }), .point_2 = .fromArray(.{ 0, 6, 3 }) },
+            }),
+        }, .{} } },
+        .{ .players = .{ .{
+            .move_frame = 4,
+            .move_total_frames = 5,
+            .position = sdk.math.Vec3.fromArray(.{ 0, 2, 0 }),
+            .rotation = 0.5 * std.math.pi,
+            .hurt_cylinders = hurtCylinders(2),
+            .hit_lines = hitLines(.{}),
+        }, .{} } },
+        .{ .players = .{ .{
+            .move_frame = 5,
+            .move_total_frames = 5,
+            .position = sdk.math.Vec3.fromArray(.{ 0, 1, 0 }),
+            .rotation = 0.5 * std.math.pi,
+            .hurt_cylinders = hurtCylinders(1),
+            .hit_lines = hitLines(.{}),
+        }, .{} } },
+        .{ .players = .{ .{
+            .move_frame = 1,
+            .move_total_frames = 2,
+            .position = sdk.math.Vec3.fromArray(.{ 0, 1, 0 }),
+            .rotation = 0.5 * std.math.pi,
+            .hurt_cylinders = hurtCylinders(1),
+            .hit_lines = hitLines(.{}),
+        }, .{} } },
+        .{ .players = .{ .{
+            .move_frame = 2,
+            .move_total_frames = 2,
+            .position = sdk.math.Vec3.fromArray(.{ 0, 1, 0 }),
+            .rotation = 0.5 * std.math.pi,
+            .hurt_cylinders = hurtCylinders(1),
+            .hit_lines = hitLines(.{}),
+        }, .{} } },
+        .{ .players = .{ .{
+            .move_frame = 1,
+            .move_total_frames = 99,
+            .position = sdk.math.Vec3.fromArray(.{ 0, 1, 0 }),
+            .rotation = 0.5 * std.math.pi,
+            .hurt_cylinders = hurtCylinders(1),
+            .hit_lines = hitLines(.{}),
+        }, .{} } },
+    };
+
+    var measurer = MoveMeasurer{};
+    for (&frames, 0..) |*frame, index| {
+        measurer.measure(frame);
+        switch (index) {
+            2 => {
+                try testing.expectEqual(1, frame.players[0].min_attack_z);
+                try testing.expectEqual(2, frame.players[0].max_attack_z);
+                try testing.expectEqual(3, frame.players[0].attack_range);
+                try testing.expectEqual(null, frame.players[0].recovery_range);
+            },
+            3, 4 => {
+                try testing.expectEqual(1, frame.players[0].min_attack_z);
+                try testing.expectEqual(3, frame.players[0].max_attack_z);
+                try testing.expectEqual(4, frame.players[0].attack_range);
+                try testing.expectEqual(null, frame.players[0].recovery_range);
+            },
+            5 => {
+                try testing.expectEqual(1, frame.players[0].min_attack_z);
+                try testing.expectEqual(3, frame.players[0].max_attack_z);
+                try testing.expectEqual(4, frame.players[0].attack_range);
+                try testing.expectEqual(3, frame.players[0].recovery_range);
+            },
+            6 => {
+                try testing.expectEqual(null, frame.players[0].min_attack_z);
+                try testing.expectEqual(null, frame.players[0].max_attack_z);
+                try testing.expectEqual(null, frame.players[0].attack_range);
+                try testing.expectEqual(3, frame.players[0].recovery_range);
+            },
+            else => {
+                try testing.expectEqual(null, frame.players[0].min_attack_z);
+                try testing.expectEqual(null, frame.players[0].max_attack_z);
+                try testing.expectEqual(null, frame.players[0].attack_range);
+                try testing.expectEqual(null, frame.players[0].recovery_range);
+            },
+        }
+    }
+}
